fba00622922dacb1ea4999f0c52cb5d9bccc0808
chuonghoai <144915081+chuonghoai@users.noreply.github.com>
2026-05-11 19:13:53 +0700

    chore: add note for design api in repo file

--- src/features/auth/repositories/authApi.repository.ts
+++ src/features/auth/repositories/authApi.repository.ts
@@ -4,6 +4,15 @@ import type { ApiResponse } from "../../../core/api/apiResponse";
 import type { LoginRequest, LoginResponse } from "../dto/login.type";
 
 export class AuthApiRepository implements AuthRepository {
+    /**
+     * POST /auth/login
+     * @params 
+     * @query
+     * @body LoginRequest
+     * @returns LoginResponse
+     * Fix:
+     *  - Bấm đăng nhập nhưng data trả về thiếu.....
+     */
     async login(data: LoginRequest): Promise<ApiResponse<LoginResponse>> {
         return apiClient.post<ApiResponse<LoginResponse>>("/auth/login", data);
     }
