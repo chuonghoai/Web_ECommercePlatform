da105790745ff226cae90e991d86cb8c5a7b7d3d
Ha Truong Giang <gianght09112005@gmail.com>
2026-05-14 21:15:33 +0700

    fix: add flow service send otp and register in auth service

--- src/features/auth/repositories/auth.repository.ts
+++ src/features/auth/repositories/auth.repository.ts
@@ -1,7 +1,11 @@
 import type { ApiResponse } from "../../../core/api/apiResponse";
 import type { LoginRequest, LoginResponse } from "../dto/login.type";
+import type { RegisterRequest } from "../dto/register.type";
+import type { OtpPurpose } from "../enums/otpPurpose.enum";
 
 export interface AuthRepository {
     login(data: LoginRequest): Promise<ApiResponse<LoginResponse>>;
+    register(data: RegisterRequest): Promise<ApiResponse<LoginResponse>>;
+    sendOtp(email: string, purpose: OtpPurpose): Promise<ApiResponse<void>>;
     logout(): Promise<ApiResponse<null>>;
 }
--- src/features/auth/repositories/authMock.repository.ts
+++ src/features/auth/repositories/authMock.repository.ts
@@ -1,6 +1,8 @@
 import type { ApiResponse } from "../../../core/api/apiResponse";
 import { UnauthorizedException } from "../../../core/exceptions/unauthorized.exception";
 import type { LoginRequest, LoginResponse } from "../dto/login.type";
+import type { RegisterRequest } from "../dto/register.type";
+import type { OtpPurpose } from "../enums/otpPurpose.enum";
 import type { AuthRepository } from "./auth.repository";
 
 export class AuthMockRepository implements AuthRepository {
@@ -23,6 +25,13 @@ export class AuthMockRepository implements AuthRepository {
         }
 
         throw new UnauthorizedException("Sai email hoặc mật khẩu");
+    }
+
+    register(data: RegisterRequest): Promise<ApiResponse<LoginResponse>> {
+        throw new Error("Method not implemented.");
+    }
+    sendOtp(email: string, purpose: OtpPurpose): Promise<ApiResponse<void>> {
+        throw new Error("Method not implemented.");
     }
 
     async logout(): Promise<ApiResponse<null>> {
