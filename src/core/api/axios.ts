61bd68e650e09e48ba778114868d3e538a605d63
tad0025 <datt32285@gmail.com>
2026-05-14 22:08:09 +0700

    fix: update axois

--- src/core/api/axios.ts
+++ src/core/api/axios.ts
@@ -5,6 +5,7 @@ import { ENV } from "../config/env.config";
 export const axiosInstance = axios.create({
     baseURL: ENV.API_URL,
     timeout: ENV.API_TIMEOUT,
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
