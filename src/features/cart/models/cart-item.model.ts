6bbed298067344e984e5efb1f706622e93837f2d
chuonghoai <144915081+chuonghoai@users.noreply.github.com>
2026-05-21 09:37:08 +0700

    feat: add dto seller info in cart features

--- src/features/cart/models/cart-item.model.ts
+++ src/features/cart/models/cart-item.model.ts
@@ -1,12 +1,20 @@
 export interface Product {
-id: string;
-name: string;
-imageUrl: string;
-price: number;
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: number;
+  seller: SellerInfo;
 }
 
 
 export interface CartItem {
   product: Product;
   quantity: number;
+}
+
+export interface SellerInfo {
+  id: string;
+  name: string;
+  avatarUrl: string;
+  averageRating: number;
 }
