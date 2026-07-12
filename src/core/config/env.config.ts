37d09332ed4f993aa1bc83799b803d74231ce826
PhamDuyVo <189071432+PhamDuyVo@users.noreply.github.com>
2026-07-12 21:00:19 +0700

    feat: implement more environment (dev, lan, preview)

--- src/core/config/env.config.ts
+++ src/core/config/env.config.ts
@@ -1,5 +1,5 @@
 export const ENV = {
-    API_URL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+    API_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
     API_TIMEOUT: Number(
         import.meta.env.VITE_API_TIMEOUT || 10000
     ),
